9bf89acb80e7bd1c90b5b0fdb48562e854036488
James Halliday <mail@substack.net>
2014-07-29 08:38:07 +1200

    adapted from level-lock example

---
+++ index.js
@@ -0,0 +1,35 @@
+var lock = require('level-lock');
+
+module.exports = function (db, key, value, cb) {
+    var unlock = lock(db, key, 'w');
+    if (!unlock) {
+        return nextErr(cb, 'LOCKED', 'key is write-locked');
+    }
+    
+    db.get(key, function (err, res) {
+        if (err && err.type !== 'NotFoundError') {
+            unlock();
+            return cb(err);
+        }
+        if (res) {
+            unlock();
+            return cb(error('EXISTS', 'key already exists'));
+        }
+        
+        db.put(key, userdata, function (err) {
+            unlock();
+            cb(err);
+        });
+    });
+};
+
+function error (code, msg) {
+    var err = new Error(msg);
+    err.code = err.type = code;
+    return err;
+}
+
+function nextErr (cb, code, msg) {
+    var err = error(code, msg);
+    process.nextTick(function () { cb(err) });
+}
---
+++ readme.markdown
@@ -0,0 +1,7 @@
+# level-insert
+
+insert a key if and only if it doesn't already exist
+
+# example
+
+
